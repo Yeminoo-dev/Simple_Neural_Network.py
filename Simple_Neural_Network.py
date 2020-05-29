2c0aacd995da47573e0b9e0aff86f0322130057e
Yeminoo-dev <66120191+Yeminoo-dev@users.noreply.github.com>
2020-05-29 20:04:40 +0630

    Update Simple_Neural_Network.py

--- Simple_Neural_Network.py
+++ Simple_Neural_Network.py
@@ -1,1 +1,6 @@
 import numpy as np
+###########################################Author : Ye Min Oo ( McE ) ###############################################################
+class Dense():
+  def __init__(self,layer,n_inputs,n_neurons,activation):
+    self.layer = layer
+    self.weights = np.random.randn(n_neurons, n_inputs)
