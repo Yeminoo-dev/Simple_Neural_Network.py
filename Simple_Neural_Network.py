93fb6cf6893ebf309da31b6ec88bcf3f2abcc3b8
Yeminoo-dev <66120191+Yeminoo-dev@users.noreply.github.com>
2020-06-24 07:11:33 +0630

    Update Simple_Neural_Network.py

--- Simple_Neural_Network.py
+++ Simple_Neural_Network.py
@@ -1,13 +1,1 @@
-#Author : Ye Min Oo
-import numpy as np
-import matplotlib.pyplot as plt
 
-class Dense():
-        def __init__(self, layer, n_inputs, n_neurons, activation):
-            self.layer = layer
-            self.weights = np.random.randn(n_neurons, n_inputs)
-            self.bias = np.zeros((n_neurons, 1)) 
-            self.activation = activation
-        def params_packing(self, w,b):
-            WL["layer" + str(self.layer)] = w 
-            bL["layer" + str(self.layer)] = b
